fb2bc7e050c96a20e47e793a32ab5ca661200edd
Vuong Nguyen <trungvuong2169@gmail.com>
2023-01-18 12:17:26 +0700

    UserController (userData -> user)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,19 +2,18 @@ import userService from "../services/userService";
 let handleLogin = async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  console.log("Email " + email);
   if (!email || !password) {
     return res.status(500).json({
       errCode: 1,
       message: "Missing Inputs parameter!!",
     });
   }
-  let userData = await userService.handleUserLogin(email, password);
-  console.log(userData);
+  let user = await userService.handleUserLogin(email, password);
+
   return res.status(200).json({
-    errCode: userData.errCode,
-    message: userData.errMessage,
-    userData,
+    errCode: user.errCode,
+    message: user.errMessage,
+    user,
   });
 };
 module.exports = {
